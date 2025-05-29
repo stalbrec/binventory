266f21952f0a640a22b3a8db34f364b133db0a4c
Steffen Albrecht <steffen.alb@posteo.de>
2025-05-29 17:44:07 +0200

    update box.html with custom qrcodes passed as base64 datastr

--- inventory/views.py
+++ inventory/views.py
@@ -1,8 +1,11 @@
 from django.views import generic
 from .models import Box, Item
 from django.shortcuts import render, redirect
+import qrcode
+import base64
+from io import BytesIO
 # Create your views here.
-
+import logging
 
 class IndexView(generic.ListView):
     template_name = "inventory/index.html"
@@ -41,9 +44,17 @@ class BoxView(BoxAwareDetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["full_url"] = self.request.build_absolute_uri(
+        full_url = self.request.build_absolute_uri(
             self.object.get_absolute_url()
         )
+        context["full_url"]=full_url
+
+        qr = qrcode.make(full_url)
+        buffer = BytesIO()
+        qr.save(buffer, format="PNG")
+        img_str = base64.b64encode(buffer.getvalue())
+        context["qr_image_data"] = f"data:image/png;base64,{img_str.decode()}"
+        logging.error(img_str)
         return context
 
 
