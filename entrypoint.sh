3b408858b1e6253e7f42fb48afe030efa9befee5
Steffen Albrecht <steffen.alb@posteo.de>
2025-06-01 10:07:13 +0200

    adding timeout to gunicorn in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-gunicorn --bind 0.0.0.0:8000 binventory.wsgi:application
+gunicorn --bind 0.0.0.0:8000 binventory.wsgi:application --timeout 60
